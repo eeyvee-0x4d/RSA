5823bea3e36217d49b8af21edc0b4c52db7735f4
Al Vincent Musa <alvincentvibar.musa@bicol-u.edu.ph>
2021-03-06 09:36:14 +0800

    added is_coprime()

--- rsa.py
+++ rsa.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 import argparse
+import math
+
+# check if two numbers is a coprime
+def is_coprime(e, totient_n):
+
+	if math.gcd(e, totient_n) == 1:
+		return True
+	else:
+		return False
+
 
 def main():
 
-	p = 0
-	q = 0
+	p = 13
+	q = 17
 	n = p * q
+
 	totient_n = (p - 1) * (q - 1)
 
 	e = 0
 
- 
+	while(e < 1  or e > totient_n and is_coprime(e, totient_n)):
+		print("Select e from 1 to {0}: ".format(totient_n))
+		e = int(input())
+		
 if __name__ == '__main__':
 	main()
